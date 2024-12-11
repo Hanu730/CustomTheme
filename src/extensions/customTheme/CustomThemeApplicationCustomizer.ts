d403b0a6d2ef6a7e0d32582e8ae0edd939bbb601
hanu370 <hanumanthu2492@gmail.com>
2024-12-11 11:39:24 +0530

    backup before 12th dec

--- src/extensions/customTheme/CustomThemeApplicationCustomizer.ts
+++ src/extensions/customTheme/CustomThemeApplicationCustomizer.ts
@@ -3,6 +3,10 @@ import {
   BaseApplicationCustomizer
 } from '@microsoft/sp-application-base';
 import { loadStyles } from '@microsoft/load-themed-styles';
+import { SPHttpClient } from '@microsoft/sp-http';
+// import * as ReactDom from 'react-dom';
+// import * as React from 'react';
+// import { SearchBox } from '@fluentui/react';
 //import { Dialog } from '@microsoft/sp-dialog';
 
 //import * as strings from 'CustomThemeApplicationCustomizerStrings';
@@ -23,176 +27,327 @@ export interface ICustomThemeApplicationCustomizerProperties {
 export default class CustomThemeApplicationCustomizer
   extends BaseApplicationCustomizer<ICustomThemeApplicationCustomizerProperties> {
 
-  public onInit(): Promise<void> {
 
-  //   const customStyles: string = `
-  //   :root {
-  //     --theme-primary-color: #333333;
-  //     --theme-secondary-color: #4CAF50;
-  //     --background-color: #f0f0f0;
-  //     --font-color: #323130;
-  //   }
+    private wrapperSelector: string = ".sideActionsWrapper-61";
+  //private placeholder: HTMLDivElement | null = null;
 
-  //   body {
-  //     background-color: var(--background-color);
-  //     color: var(--font-color);
-  //     font-family: "Segoe UI", Arial, sans-serif;
-  //   }
+  public async onInit(): Promise<void> {
+   this.injectSearchBox();
+   const isSiteVisitor = await this.isUserInGroup('Websyn-Intranet-UAT Visitors');
 
-  //   a {
-  //     color: var(--theme-primary-color);
-  //   }
-
-  //   .ms-Button {
-  //     background-color: var(--theme-primary-color);
-  //     border-color: var(--theme-primary-color);
-  //     color: white;
-  //   }
-
-  //   .ms-Button:hover {
-  //     background-color: var(--theme-secondary-color);
-  //   }
-  // `;
-
-  this.injectQuickLaunchStyles();
-
-  return Promise.resolve();
-  // Inject the custom styles into the page head
-  // const styleElement: HTMLStyleElement = document.createElement('style');
+    const isSiteOwner = await this.isUserInGroup('Websyn-Intranet-UAT Owners');
+    const isSiteMember = await this.isUserInGroup('Websyn-Intranet-UAT Members');
+    console.log(isSiteVisitor);
+    if(isSiteOwner || isSiteMember){
+        this.injectQuickLaunchStylesforMembers()
+      
+    }else{
+        this.injectQuickLaunchStylesforVisitors();
+    }
   
-  // styleElement.type = 'text/css';
-  // styleElement.innerHTML = customStyles;
-  // document.head.appendChild(styleElement);
-    // const style = document.createElement('style');
-    // style.innerHTML = `
-    //   :root {
-    //     --primary-color: #ff6347;
-    //     --accent-color: #4CAF50;
-    //   }
-    //  body {
-    //     background-color: #f0f0f0;
-    //   }
-    // `;
-    // document.head.appendChild(style);
-
-
-    // Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
-
-    // let message: string = this.properties.testMessage;
-    // if (!message) {
-    //   message = '(No properties were provided.)';
-    // }
-
-    // Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`).catch(() => {
-    //   /* handle error */
-    // });
-
-    // return Promise.resolve();
+  return Promise.resolve();
+  
   }
 
-  private injectQuickLaunchStyles(): void {
-    // const customCSS = `
-    // /* Style the Quick Launch container */
-    // .od-Nav-root {
-    //     background-color: #f4f4f4; /* Light gray background */
-    //     border-right: 2px solid #0078d4; /* Add a border on the right */
-    // }
+  private injectQuickLaunchStylesforVisitors(): void {
 
-    // /* Style individual links */
-    // .od-Nav-link {
-    //     color: #0078d4; /* Custom link color */
-    //     font-size: 14px; /* Custom font size */
-    //     padding: 8px 12px; /* Custom padding */
-    //     transition: background-color 0.3s;
-    // }
     
-    // /* Hover effect for links */
-    // .od-Nav-link:hover {
-    //     background-color: #e1e1e1; /* Light gray hover background */
-    //     text-decoration: none;
-    // }
-
-    // /* Style selected link */
-    // .od-Nav-link.is-selected {
-    //     background-color: #0078d4; /* Selected background color */
-    //     color: white; /* Selected text color */
-    //     font-weight: bold;
-    // }
-    // `;
-    // const customCSS = `
-    // /* Style the site header container */
-    // .sp-site-header {
-    //     background-color: #0078d4; /* Custom background color */
-    //     color: white; /* Custom text color */
-    //     border-bottom: 2px solid #005a9e; /* Add a border at the bottom */
-    // }
-
-    // /* Style the header title */
-    // .sp-site-header .od-SiteHeader-title {
-    //     font-size: 20px; /* Custom font size */
-    //     font-weight: bold; /* Bold font weight */
-    // }
-
-    // /* Style header icons (e.g., settings, notifications) */
-    // .sp-site-header .ms-Button-icon {
-    //     color: white; /* Change icon color */
-    // }
-
-    // /* Add hover effect for buttons */
-    // .sp-site-header .ms-Button:hover {
-    //     background-color: #005a9e; /* Hover background color */
-    // }
-    // `;
-
-    const customCSS = `
-    /* Style the horizontal navigation container */
-    .ms-HorizontalNav {
-        background-color: #f26724; /* #f4f4f4 Light gray background */
-        padding: 10px 20px; /* Add some padding */
-        //border-bottom: 2px solid #0078d4; /* Add a bottom border */
-    }
-    // .ms-HorizontalNavItem-linkText{
-    // color:white;
-    // }
-    .ms-HorizontalNavItem-link{
-    color:white;
-    font-weight: bold; 
-    }
-    .f1vjtpfu .ms-HorizontalNavItem-linkText {
-       font-weight: bold; 
-    /* font-weight: var(--fontWeightCustomFont500, var(--fontWeightRegular)); */
-}
-    .f1ig8277 .ms-HorizontalNavItem-linkText.is-selected {
-     font-weight: bold; 
-}
    
-    /* Style individual navigation items */
-    .ms-HorizontalNavItem {
-        font-size: 16px; /* Custom font size */
-        font-weight: bold; /* Bold text */
-        color: #ffffff; /* Link color #0078d4*/
-        margin-right: 15px; /* Space between items */
-        text-decoration: none; /* Remove underline */
-        transition: color 0.3s, background-color 0.3s; /* Add smooth hover effect */
-    }
+ const customCSS=`
+ div#SuiteNavPlaceholder {
+    display: none;
+}
+div#spCommandBar {
+    display: none;
+}
+.a_b_50a7110f:not(.e_b_50a7110f):not(.o_b_50a7110f) {
+    margin-top: 0;
+}
+ .titleAndNavWrapper-112.titleAndNavWrapper-112.titleAndNavWrapper-112.titleAndNavWrapper-112.titleAndNavWrapper-112 {
+    display: inline-block;
+}
+    .ms-HorizontalNavItems {
+    float: right;
+}
 
-    /* Hover effect for navigation items */
-    .ms-HorizontalNavItem:hover {
-        background-color: #e1e1e1; /* Light gray background on hover */
-        color:white; /* Darker blue text on hover */
-     }
-   
-    /* Style active navigation item */
-    .ms-HorizontalNavItem .is-selected {
-        // background-color: #0078d4; /* Blue background */
-        color: #ffffff; /* White text */
-        border-bottom: 2px solid black
-       
+    .mainHeader-108.mainHeader-108.mainHeader-108.mainHeader-108.mainHeader-108 {
+    padding-top: 15px;
+}
+// .fh2jcsl .ms-ButtonShim-flexContainer {
+//     display: none;
+//     diplay:block;
+// }
+    .ms-Stack.css-208 > div {
+    border-radius: 0 !important;
+}
+    //  .sideActionsWrapper-61.sideActionsWrapper-61.sideActionsWrapper-61.sideActionsWrapper-61.sideActionsWrapper-61 {
+    //      display: none;
+    // }
+.announcementWebPart_d7c8a9f1 h3 {
+    border-radius: 0 !important;
+}
+.announcementWebPart_d7c8a9f1 {
+    padding: 0;
+    border: 0;
+}
+  
+ .quickLinksHeader_5d356418 {
+    background: transparent;
+    color: #000;
+    font-size: 18px; background-color: #f26724;
+    border-radius: 0;
+}
+    .actionsSubcell {
+         display: none;
     }
-    `;
+    .ms-Stack > div {
+    border-radius: 0 !important;
+}
+.css-150, .ms-Stack span {
+    font-size: 14px;
+}
+.a_b_50a7110f:not(.e_b_50a7110f):not(.o_b_50a7110f) {
+    padding-left: 0;
+    margin-bottom:0;
+}
+.homeWebpart_5d356418 {  
+    padding: 0;
+    border: 0;
+}
+.titleSubcell-113.titleSubcell-113.titleSubcell-113.titleSubcell-113.titleSubcell-113 {
+    float: left;
+}
+[dir=ltr] .ms-HorizontalNavItem:first-child {
+    margin-left: 29px;
+    margin-right: 16px !important;
+}
+[dir=ltr] .ms-HorizontalNavItem:not(:last-child) {
+    margin-right: 1px;
+}
+.fwiecsv #SiteHeaderTitle a {
+    font-size: 24px;
 
+}
+.fpnq9et .ms-HorizontalNavItem-linkText.is-selected {
+    font-size: 15px;
+   color: #333333;
+   font-weight:600;
+  
+}
+    .mm_am_9f38462c img {
+    object-fit: cover !important;
+    width:100%;
+}
+    @media (min-width: 1024px) {
+    .a_c_50a7110f:not(.e_c_50a7110f).t_c_50a7110f {
+        padding-right: 0;
+    }
+    .a_c_50a7110f:not(.e_c_50a7110f).r_c_50a7110f {
+        padding-left: 0;
+    }
+}
+@media (min-width: 640px) {
+    .a_c_50a7110f:not(.e_c_50a7110f).s_c_50a7110f {
+        width: 50%;
+        padding-right:0px;
+    }
+        .g_a_50a7110f .a_c_50a7110f:not(.e_c_50a7110f).s_c_50a7110f:first-child {
+    width: 88%;
+    padding-right: 0;
+}
+}
+.fv4b208 .ms-HorizontalNavItem-linkText {
+   font-size: 15px;
+   color: #333333;
+   font-weight:600;
+}`;
     loadStyles(customCSS);
-    //loadStyles(customCSS);
+   
 }
+
+private injectQuickLaunchStylesforMembers(): void {
+
+
+const customCSS=`
+div#SuiteNavPlaceholder {
+  display: none;
+}
+.a_b_50a7110f:not(.e_b_50a7110f):not(.o_b_50a7110f) {
+  margin-top: 0;
+}
+.titleAndNavWrapper-112.titleAndNavWrapper-112.titleAndNavWrapper-112.titleAndNavWrapper-112.titleAndNavWrapper-112 {
+  display: inline-block;
+}
+.titleSubcell-113.titleSubcell-113.titleSubcell-113.titleSubcell-113.titleSubcell-113 {
+  float: left;
+}
+[dir=ltr] .ms-HorizontalNavItem:first-child {
+  margin-left: 29px;
+  margin-right: 16px !important;
+}
+[dir=ltr] .ms-HorizontalNavItem:not(:last-child) {
+  margin-right: 1px;
+}
+.fwiecsv #SiteHeaderTitle a {
+  font-size: 20px;
+
+}
+.fpnq9et .ms-HorizontalNavItem-linkText.is-selected {
+  font-size: 15px;
+   color: #333333;
+   font-weight:600;
+  
+  
+}
+     .ms-HorizontalNavItems {
+    float: right;
+}
+    .mainHeader-108.mainHeader-108.mainHeader-108.mainHeader-108.mainHeader-108 {
+    padding-top: 15px;
+}
+// .fh2jcsl .ms-ButtonShim-flexContainer {
+//     display: none;
+//     diplay:block;
+// }
+    .ms-Stack > div {
+    border-radius: 0 !important;
+}
+.css-150, .ms-Stack span{
+    font-size: 14px;
+}
+.a_b_50a7110f:not(.e_b_50a7110f):not(.o_b_50a7110f) {
+    padding-left: 0;
+     margin-bottom:0;
+}
+    .ms-Stack.css-208 > div {
+    border-radius: 0 !important;
+}
+    // .sideActionsWrapper-61.sideActionsWrapper-61.sideActionsWrapper-61.sideActionsWrapper-61.sideActionsWrapper-61 {
+    //      display: none;
+    // }
+.announcementWebPart_d7c8a9f1 h3 {
+    border-radius: 0 !important;
+}
+.announcementWebPart_d7c8a9f1 {
+    padding: 0;
+    border: 0;
+}
+ .quickLinksHeader_5d356418 {
+    background: transparent;
+    color: #000;
+    font-size: 18px; background-color: #f26724;
+    border-radius: 0;
+}
+.homeWebpart_5d356418 {  
+    padding: 0;
+    border: 0;
+}
+  .mm_am_9f38462c img {
+  object-fit: cover !important;
+  width:100%;
+}
+  @media (min-width: 1024px) {
+  .a_c_50a7110f:not(.e_c_50a7110f).t_c_50a7110f {
+      padding-right: 0;
+  }
+  .a_c_50a7110f:not(.e_c_50a7110f).r_c_50a7110f {
+      padding-left: 0;
+  }
+}
+@media (min-width: 640px) {
+  .a_c_50a7110f:not(.e_c_50a7110f).s_c_50a7110f {
+      width: 50%;
+      padding-right:0px;
+  }
+      .g_a_50a7110f .a_c_50a7110f:not(.e_c_50a7110f).s_c_50a7110f:first-child {
+  width: 88%;
+  padding-right: 0;
+}
+}
+.fv4b208 .ms-HorizontalNavItem-linkText {
+  font-size: 15px;
+   color: #333333;
+   font-weight:600;
+}`;
+  loadStyles(customCSS);
+  //loadStyles(customCSS);
+}
+
+
+private async isUserInGroup(groupName: string): Promise<boolean> {
+    var isvisitor= false;
+    const groupname=groupName;
+
+    debugger;
+        //var result=null;
+
+  var endpoint = `${this.context.pageContext.web.absoluteUrl}/_api/web/sitegroups/getbyname('${groupName}')/users?$filter=Id eq ${this.context.pageContext.legacyPageContext.userId}`;
+  if(groupname =="Websyn-Intranet-UAT Visitors"){
+   endpoint = `${this.context.pageContext.web.absoluteUrl}/_api/web/sitegroups/getbyname('${groupName}')/users`;
+   const response = await this.context.spHttpClient.get(endpoint, SPHttpClient.configurations.v1);
+   const result = await response.json();
+   result.value.forEach((element:any )=> {
+    if(element.Title=="Everyone except external users")
+        isvisitor=true;
+   });
+   return isvisitor;
+  }else{
+  const response = await this.context.spHttpClient.get(endpoint, SPHttpClient.configurations.v1);
+  const result = await response.json();
+
+  return result.value.length > 0;
+  } // If user is in the group, ?$filter=Id eq ${this.context.pageContext.legacyPageContext.userId} result.value will have data
+}
+
+private injectSearchBox = (): void => {
+    const wrapperElement = document.querySelector(this.wrapperSelector) as HTMLElement;
+    const ele=document.querySelectorAll("[class^='sideActionsWrapper']");
+    console.log(ele);
+    
+    document.querySelectorAll("[class^='sideActionsWrapper']").forEach(e=>{e.remove()});
+
+    if (wrapperElement) {
+      // Remove all children of the wrapper
+      while (wrapperElement.firstChild) {
+       
+      }
+
+      //document.querySelectorAll("[class^='sideActionsWrapper']").forEach(e=>{e.remove()});
+      // Get all elements with the class name
+      //document.querySelectorAll('#spSiteHeader > div > div.fui-FluentProvider.fui-FluentProvider3.___1c1870d.f1ym3bx4.f1u2r49w.f6sp5hn.f1mo0ibp.fjoy568.ff5ikls.f1qumt79.f1bxpd7w > div > div > div.sideActionsWrapper-61 > div.actionsSubcell-63').forEach(e=>{e.remove()});
+
+
+      // Create a placeholder div for the search box
+    //   this.placeholder = document.createElement("div");
+    //   wrapperElement.appendChild(this.placeholder);
+
+    //   // Create the textbox element using TypeScript
+    //   const inputElement: HTMLInputElement = document.createElement("input");
+    //   inputElement.type = "text";
+    //   inputElement.placeholder = "Search...";
+    //   inputElement.style.padding = '5px';
+    //   inputElement.style.width = '200px';
+    //   inputElement.style.borderRadius = '4px';
+
+    //   // Add an event listener for handling input changes
+    //   inputElement.addEventListener("input", (e) => {
+    //     console.log("Input value:", (e.target as HTMLInputElement).value);
+    //   });
+
+    //   // Append the textbox to the placeholder
+    //   this.placeholder.appendChild(inputElement);
+      // Render a React SearchBox component
+
+    }
+  };
+
+// private async validateUser(): Promise<boolean> {
+//   const isSiteOwner = await this.isUserInGroup('Websyn-Intranet-UAT Owners');
+//   const isSiteMember = await this.isUserInGroup('Websyn-Intranet-UAT Members');
+//   const isSiteVisitor = await this.isUserInGroup('Websyn-Intranet-UAT Visitors');
+
+//   return isSiteOwner || isSiteMember||isSiteVisitor;
+// }
+
 
 }
